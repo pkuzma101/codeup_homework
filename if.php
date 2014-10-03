153e39eaa37dcfe733a3eccb130bd09a542b96ab
pkuzma101 <kuzma.paul@gmail.com>
2014-10-03 11:25:10 -0500

    Paul K.'s work for exercise 3.1.3

--- if.php
+++ if.php
@@ -2,46 +2,48 @@
 
 $a = 5;
 $b = 10;
-$c = '10';
+$c = 20;
 
 if($a < $b) {
  	// output the appropriate result
- 	echo "$a is less than $b\n";
+ 	echo "{$a} is less than {$b}\n";
+ }
+ else {
+ 	echo "{$a} is greater than {$b}\n";
  }
 
-if($b > $a) {
- 	// output the appropriate result
-	echo "$b is greater than $a\n";
- }
-
-//if($b !== $c) {
-//	echo "$b is not identical to $c";
-//}
+// if($b > $a) {
+//  	// output the appropriate result
+// 	echo "$b is greater than $a\n";
+//  }
 
 if($b >= $c) {
 	//output the appropriate result
-	echo "$b is greater than or equal to $c\n";
+	echo "{$b} is greater than or equal to {$c}\n";
+}
+else {
+	echo "{$b} is less than or equal to {$c}\n";
 }
 
-if($b <= $c) {
-	//output the appropriate result
-	echo "$b is less than or equal to $c\n";
-}
+// if($b <= $c) {
+// 	//output the appropriate result
+// 	echo "$b is less than or equal to $c\n";
+// }
 
 if($b == $c) {
-	echo "$b is equal to $c\n";
+	echo "{$b} is equal to {$c}\n";
+}
+elseif($b === $c) {
+	echo "{$b} is not identical to {$c}\n";
 }
 
-if($b === $c) {
-	echo "$b is not identical to $c\n";
+elseif($b != $c) {
+	echo "{$b} is not equal to {$c}\n";
 }
 
-if($b != $c) {
-	echo "$b is not equal to $c\n";
-}
-
-if($b !== $c) {
-	echo "$b is not identical to $c\n";
+//if($b !== $c) {
+else {
+	echo "{$b} is not identical to {$c}\n";
 }
 
 ?>

d887e923b957d0dcb6d6a7d457359afa478522c0
pkuzma101 <kuzma.paul@gmail.com>
2014-10-03 11:30:24 -0500

    Made changes to dice game

--- roll_dice.php
+++ roll_dice.php
@@ -1,8 +1,9 @@
 <?php
 define ('SIDES_OF_DICE', 6);
-
+// This is where it decides what you rolled
 $roll = mt_rand(1, SIDES_OF_DICE);
-
+// This is where it will tell you what you rolled
 echo "You rolled {$roll}\n";
 
 ?>
+

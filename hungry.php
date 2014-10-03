746e189699cc4d70f0fadba7fdf09bb99055e4a9
pkuzma101 <kuzma.paul@gmail.com>
2014-10-03 12:59:43 -0500

    week 1 homework - exercise 8 and 9

---
+++ hungry.php
@@ -0,0 +1,18 @@
+<?php
+
+$loggedIn = TRUE;
+
+echo $loggedInStatus = $loggedIn ? 'You are logged in' : 'You are not logged in';
+
+// $hungry = TRUE;
+// $foodAvailable = TRUE;
+// $haveMoney = FALSE;
+
+// if($hungry && $foodAvailable || $haveMoney) {
+// 	echo "We are fed and good to go\n";
+// }
+// else {
+// 	echo "Not hungry, but thank you!\n";
+// }
+
+?>

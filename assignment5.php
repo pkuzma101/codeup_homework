6f2b1e009f1b3ceab16cf229ff6252058ffaa659
pkuzma101 <kuzma.paul@gmail.com>
2014-10-03 14:48:43 -0500

    adding all files so I can clean up

---
+++ assignment5.php
@@ -0,0 +1,7 @@
+<?php
+define('BIRTH_CITY', 'Indianapolis');
+
+// 2. Yes, because I knew it wouldn't work.
+// 3. Because I had already made 'BIRTH_CITY' a constant
+?>
+
---
+++ assignment6.php
@@ -0,0 +1,37 @@
+<?php
+
+$primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+	echo $primeNumbers[3];
+
+$car = [
+	"Emily" => [
+		"year" => 2004
+		"make" => "Ford"
+		"model" => "Explorer"
+		"color" => "grey"
+		"mileage" => 140000
+	],
+
+	"Paul" => [
+		"year" => 2006
+		"make" => "Honda"
+		"model" => "Civic"
+		"color" => "blue"
+		"mileage" => 120000	
+	],
+
+	"Evie" => [
+		"year" => 2008
+		"make" => "Honda"
+		"model" => "Car"
+		"color" => "black"
+		"mileage" => 100000
+	],
+];
+
+// 7. php> echo "Evie's car is " . $cars ['Evie']['make'];
+
+// 8.  So you can put lists of information within lists of information.  Such as information about
+//	   different customers
+
+?>
